dcf21801c26b19a94ec90c9cfa4bf34077e4729d
Chris Cai <caihuijigood@gmail.com>
2015-01-14 17:35:33 +0800

    fix

--- test/test-query.js
+++ test/test-query.js
@@ -8,7 +8,7 @@ var mongoUrl = "mongodb://betterjs:betterjs4imweb@localhost:27017/badjs";
 
 var mongoDB;
 // Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(mongoUrl, function(err, db) {
     if(err){
         console.log("failed connect to server");
     }else {
@@ -26,24 +26,12 @@ MongoClient.connect(url, function(err, db) {
 
 
     mongoDB.collection('badjslog_' + 991).find(queryJSON , function (error,cursor){
-        res.writeHead(200, {
-            'Content-Type': 'text/json'
-        });
-
-        res.write('[');
-        var first = true;
         cursor.sort({'date' : -1}).skip(json.index * limit).limit(limit).forEach(function(error,item){
-            if(item){
-                delete item.all;
-                item.date = item.date -0;
-                res.write( (first ? '' : ',' ) + JSON.stringify(item));
-            }else {
-                res.write(']');
-                res.end();
-                return ;
-            }
-            first = false;
-
+           if(error){
+               console.log("error : " + error);
+           }else if(){
+               console.log(JSON.stringify(item));
+           }
         });
 
     });
