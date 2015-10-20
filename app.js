1cf5de2f889c59bad464774ef70643b4c1ac04b8
Chris Cai <caihuijigood@gmail.com>
2015-10-20 11:23:39 +0800

    use absolute path require project.json

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var log4js = require('log4js'),
     logger = log4js.getLogger();
 
+var path = require("path");
+
 
 
 var argv = process.argv.slice(2);
 if(argv.indexOf('--project') >= 0){
-    GLOBAL.pjconfig =  require('./project.debug.json');
+    GLOBAL.pjconfig =  require(path.join(__dirname , 'project.debug.json'));
 }else {
-    GLOBAL.pjconfig = require('./project.json');
+    GLOBAL.pjconfig = require(path.join(__dirname , 'project.json'));
 }
 
 if(argv.indexOf('--debug') >= 0){
